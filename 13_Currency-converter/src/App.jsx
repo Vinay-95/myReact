23df31fb8d78613797eb0d84f5d8b99274dd0336
Vinay-95 <vnayvs@gmail.com>
2024-05-24 15:42:16 +0530

    New changes

---
+++ 13_Currency-converter/src/App.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState, useRef } from "react";
+
+import "./App.css";
+
+function App() {
+  const [amount, setAmount] = useState("");
+  const [fromCurrency, setFromCurrency] = useState("INR");
+  const [toCurrency, setToCurrency] = useState("USD");
+  const [output, setOutput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(
+    function () {
+      async function Converter() {
+        setIsLoading(true);
+        const res = await fetch(
+          // `https://api.frankfurter.app/latest?amount=100&from=INR&to=USD`
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
+        );
+        const data = await res.json();
+        setOutput(data.rates[toCurrency]);
+        setIsLoading(false);
+      }
+      if (fromCurrency === toCurrency) {
+        return setOutput(amount);
+      }
+      Converter();
+    },
+    [amount, fromCurrency, toCurrency]
+  );
+
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setAmount("");
+          setOutput("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+
+      return () => document.removeEventListener("keydown", callback);
+    },
+    [setAmount]
+  );
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+        // disabled={isLoading}
+        ref={inputEl}
+      />
+      <select
+        value={fromCurrency}
+        onChange={(e) => setFromCurrency(e.target.value)}
+        // disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
